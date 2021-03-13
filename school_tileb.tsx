13e33eabb448eb3a407727bf418a0350bd5aa282
Louis Caillé <l.caille@thecodingmachine.com>
2021-03-13 11:16:33 +0100

    school

---
+++ school_tileb.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="school_tileb" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="school_tileb.png" width="645" height="645"/>
+</tileset>
