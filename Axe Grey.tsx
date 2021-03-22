0231d5498d8a5170b6c92fe00088bd80c83ebb9c
Louis Caillé <l.caille@thecodingmachine.com>
2021-03-22 21:14:28 +0100

    school

---
+++ Axe Grey.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Axe Grey" tilewidth="32" tileheight="32" tilecount="18" columns="9">
+ <image source="Axe Grey.png" width="288" height="64"/>
+</tileset>
---
+++ Weapon Spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="Weapon Spritesheet" tilewidth="32" tileheight="32" tilecount="112" columns="14">
+ <image source="Weapon Spritesheet.png" width="470" height="262"/>
+</tileset>
