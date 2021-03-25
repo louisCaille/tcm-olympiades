6ec317e262c29c148a1b6d9f6f93db9f44a11c41
Louis Caillé <l.caille@thecodingmachine.com>
2021-03-25 18:49:29 +0100

    jitsi rome

--- modern_tileset.tsx
+++ modern_tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="modern_tileset" tilewidth="32" tileheight="32" tilecount="2320" columns="40">
+<tileset version="1.5" tiledversion="1.5.0" name="modern_tileset" tilewidth="32" tileheight="32" tilecount="2320" columns="40">
  <image source="modern_tileset.png" width="1280" height="1856"/>
 </tileset>
